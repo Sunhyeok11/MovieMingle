c6053243264b1079d039a953eeead5139d019fab
SunHyeok <cghhld@naver.com>
2024-03-17 22:43:13 +0900

    Initial commit

---
+++ mingle/src/test/java/movie/mingle/MingleApplicationTests.java
@@ -0,0 +1,13 @@
+package movie.mingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
