08458ee761e348743991c3fb0f0698fb3b6b9182
SunHyeok <cghhld@naver.com>
2024-03-25 18:34:40 +0900

    회원서비스 생성

---
+++ mingle/src/main/java/movie/mingle/service/MemberService.java
@@ -0,0 +1,46 @@
+package movie.mingle.service;
+
+import org.springframework.stereotype.Service;
+import movie.mingle.repository.MemberRepository;
+import movie.mingle.domain.Member;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원 가입 로직
+    public Member signUp(Member member) {
+        // 회원 정보 저장
+        return memberRepository.save(member);
+    }
+
+    // 회원 정보 조회 로직
+    public Member getMember(Long memberId) {
+        // 회원 정보 조회
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new RuntimeException("Member not found"));
+    }
+
+    // 회원 정보 수정 로직
+    public Member updateMember(Long memberId, Member updatedMember) {
+        // 기존 회원 정보 조회
+        Member existingMember = getMember(memberId);
+        // 수정된 회원 정보 업데이트
+        existingMember.setUsername(updatedMember.getUsername());
+        existingMember.setEmail(updatedMember.getEmail());
+        existingMember.setPhoneNumber(updatedMember.getPhoneNumber());
+        // 업데이트된 회원 정보 저장
+        return memberRepository.save(existingMember);
+    }
+
+    // 회원 탈퇴 로직
+    public void deleteMember(Long memberId) {
+        // 회원 정보 삭제
+        memberRepository.deleteById(memberId);
+    }
+}
