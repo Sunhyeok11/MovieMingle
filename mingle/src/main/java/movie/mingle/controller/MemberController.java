d9d1a9d79fd9b0592ae07ef815ede9ab4d6ad420
SunHyeok <cghhld@naver.com>
2024-03-25 18:32:19 +0900

    memberDto 및 controller 로직 생성

---
+++ mingle/src/main/java/movie/mingle/controller/MemberController.java
@@ -0,0 +1,11 @@
+package movie.mingle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/Member")
+public class MemberController {
+
+}
---
+++ mingle/src/main/java/movie/mingle/dto/MemberDto.java
@@ -0,0 +1,16 @@
+package movie.mingle.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDto {
+
+    private String username;
+    private String email;
+    private String phoneNumber;
+    private String password;
+
+
+}
