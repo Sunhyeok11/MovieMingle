89e1016d6cd8ae82cb2ef0883dbd475959e4ca0f
SunHyeok <cghhld@naver.com>
2024-04-30 21:38:26 +0900

    정리

--- mingle/src/main/java/movie/mingle/service/OrderCancellationService.java
+++
@@ -1,6 +0,0 @@
-package movie.mingle.service;
-
-public interface OrderCancellationService {
-    void cancelOrder(Long orderId); // 주문 취소
-}
-
--- mingle/src/main/java/movie/mingle/service/OrderCreationService.java
+++
@@ -1,7 +0,0 @@
-package movie.mingle.service;
-
-import movie.mingle.dto.OrderDto;
-
-public interface OrderCreationService {
-    void createOrder(OrderDto orderDto);
-}
